ffc2750f1f86d4d209ed977ad3302cc3cdc9f84e
shwetank00001 <mshwetank@gmail.com>
2023-08-28 11:36:14 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+// cssbaseline resets the css
+// themeprovider gives the ability to pass the theem to mui
+
 
 function App() {
+  const [ theme, colorMode] = useMode()
   return (
-    <div className="App">
-      dash app
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
+        <div className="App">
+            <main className="content">
+
+            </main>
+        </div>
+      </ThemeProvider>
+
+    </ColorModeContext.Provider>
+
   );
 }
 
